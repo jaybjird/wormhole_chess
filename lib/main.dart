7545c28337df98914bda425d79638b88cb4afb0c
jdh9862 <jdh9862@gmail.com>
2024-08-01 19:38:41 -0400

    Create New Game dialog

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:wormhole_chess/model/game_board.dart';
+import 'package:go_router/go_router.dart';
 
 import 'firebase_options.dart';
+import 'widgets/game_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,26 +17,39 @@ void main() async {
   runApp(const MyApp());
 }
 
+final GoRouter _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const LandingScreen(),
+    ),
+    GoRoute(
+      path: '/games',
+      builder: (context, state) => const GameScreen(),
+    ),
+  ]
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
       // debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      routerConfig: _router,
     );
   }
 }
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<LandingScreen> createState() => _LandingScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LandingScreenState extends State<LandingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,34 +70,24 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 20),
               ElevatedButton.icon(
-                onPressed: () {},
+                onPressed: () => showDialog(context: context, builder: (ctx) => NewGameDialog()),
                 icon: const Icon(FontAwesomeIcons.chessBoard),
                 label: const Text("New Game"),
               ),
-              StreamBuilder<User?>(stream: FirebaseAuth.instance.authStateChanges(), builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                }
-
-                if (snapshot.hasData) {
-                  print(snapshot.data?.photoURL);
-                  return ElevatedButton.icon(
-                    onPressed: () => FirebaseAuth.instance.signOut(),
-                    icon: const Icon(FontAwesomeIcons.personThroughWindow),
-                    label: const Text("Sign Out"),
-                  );
-                }
-
-                if (snapshot.hasData) {
-                  // TODO: Replace with error toast
-                  print(snapshot.error);
-                }
-                return ElevatedButton.icon(
+              AuthStreamBuilder(
+                onWaiting: () => const CircularProgressIndicator(),
+                onHasData: () => ElevatedButton.icon(
+                  onPressed: () => FirebaseAuth.instance.signOut(),
+                  icon: const Icon(FontAwesomeIcons.personThroughWindow),
+                  label: const Text("Sign Out"),
+                ),
+                onNoData: () => ElevatedButton.icon(
                   onPressed: () => AuthService().signInWithGoogle(),
                   icon: const Icon(FontAwesomeIcons.google),
                   label: const Text("Sign In With Google"),
-                );
-              }),
+                ),
+                onError: (error) => print("$error"), // TODO: log error and toast user
+              ),
             ],
           ),
         ),
@@ -99,5 +105,88 @@ class AuthService {
       idToken: authentication?.idToken,
     );
     return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
+
+class AuthStreamBuilder extends StatelessWidget {
+  final Widget Function() onWaiting, onHasData, onNoData;
+  final void Function(Object)? onError;
+
+  const AuthStreamBuilder({
+    super.key,
+    required this.onWaiting,
+    required this.onHasData,
+    required this.onNoData,
+    this.onError,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) return onWaiting();
+        if (snapshot.hasData) return onHasData();
+        if (snapshot.hasError && snapshot.error != null && onError != null) {
+          onError!(snapshot.error!);
+        }
+        return onNoData();
+      },
+    );
+  }
+}
+
+class NewGameDialog extends StatefulWidget {
+  const NewGameDialog({super.key});
+
+  @override
+  State<NewGameDialog> createState() => _NewGameDialogState();
+}
+
+class _NewGameDialogState extends State<NewGameDialog> {
+  bool isFourPlayer = false;
+
+  set fourPlayer(bool selection) => setState(() => isFourPlayer = selection);
+
+  void _startGame(BuildContext context) {
+    // TODO: create a new game with the given state and pass the ID to game screen
+    context.go('/games');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("New Game"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              const Text("Players"),
+              const Spacer(),
+              SegmentedButton<bool>(
+                selected: {isFourPlayer},
+                segments: const [
+                  ButtonSegment<bool>(value: false, label: Text("2")),
+                  ButtonSegment<bool>(value: true, label: Text("4")),
+                ],
+                onSelectionChanged: (selection) => fourPlayer = selection.first,
+              ),
+            ],
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => _startGame(context),
+          child: const Text('Start Game'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text('Cancel'),
+        ),
+      ],
+    );
   }
 }
